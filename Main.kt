f49b4b1d5f4b98370f6354130a9f0e8ba857bbb0
MoellerGutten <93318104+MoellerGutten@users.noreply.github.com>
2024-02-03 14:11:47 +0100

    Test calculator for learning language

--- Main.kt
+++ Main.kt
@@ -1,4 +1,31 @@
-fun main(){
-    println("Hello World")
+fun addition(a: Double, b: Double): Double {
+    return a + b
 }
 
+fun subtraction(a: Double, b: Double): Double {
+    return a - b
+}
+
+fun multiplication(a: Double, b: Double): Double {
+    return a * b
+}
+
+fun division(a: Double, b: Double): Double {
+    return String.format("%.3f", (a / b)).toDouble()
+}
+
+fun main() {
+    print("Enter a number: ")
+    val input1: String = readln()
+
+    print("Enter a second number: ")
+    val input2: String = readln()
+
+    val a = input1.toDouble()
+    val b = input2.toDouble()
+
+    println(addition(a, b))
+    println(subtraction(a, b))
+    println(multiplication(a, b))
+    println(division(a, b))
+}
