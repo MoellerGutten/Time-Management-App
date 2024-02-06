bd457f55eabc5622e7b27f359e3cd6cda8d6ac14
MoellerGutten <93318104+MoellerGutten@users.noreply.github.com>
2024-02-06 18:32:55 +0100

    Update on the proper calculator (Parsing, Handling left to right and +, -)

--- Main.kt
+++ Main.kt
@@ -1,3 +1,19 @@
+
+fun parseInput(input : String): List<String> {
+    val reg = Regex("(?=[+-])")
+    val parsedInput: List<String> = input.split(reg)
+    var newList: MutableList<String> = parsedInput.toMutableList()
+    newList.removeAt(0)
+
+    return newList
+}
+
+fun findOperators(listToCheck : MutableList<String>): MutableList<String> {
+    println(listToCheck)
+
+    return listToCheck
+}
+
 fun addition(a: Double, b: Double): Double {
     return a + b
 }
@@ -16,16 +32,24 @@ fun division(a: Double, b: Double): Double {
 
 fun main() {
     print("Enter a number: ")
-    val input1: String = readln()
+    val input: String = readln()
+    val result = parseInput(input)
 
-    print("Enter a second number: ")
-    val input2: String = readln()
+    println(result)
 
-    val a = input1.toDouble()
-    val b = input2.toDouble()
 
-    println(addition(a, b))
-    println(subtraction(a, b))
-    println(multiplication(a, b))
-    println(division(a, b))
+    if (result[1][0] == '+') {
+        println(addition(result[0].toDouble(), result[1].toDouble()))
+    }
+    if (result[1][0] == '-') {
+        println(subtraction(result[0].take(1).toDouble(), result[1].take(1).toDouble()))
+    }
+
+    //val a = input1.toDouble()
+    //val b = input2.toDouble()
+
+    //println(addition(a, b))
+    //println(subtraction(a, b))
+    //println(multiplication(a, b))
+    //println(division(a, b))
 }
